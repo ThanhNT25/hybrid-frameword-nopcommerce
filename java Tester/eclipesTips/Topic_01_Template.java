22b6d0e8bb009cfd9916423da5fdbec9a21612ee
Nguyen Thi Thanh <nguyen.thi.thanhb@b121451-lt.sun-asterisk.com>
2022-06-14 23:24:00 +0700

    commit buid basepage class

---
+++ java Tester/eclipesTips/Topic_01_Template.java
@@ -0,0 +1,9 @@
+package eclipesTips;
+
+public class Topic_01_Template {
+	public static void main(String [] args) {
+		System.out.println();
+		
+	}
+
+}
---
+++ java Tester/javaBasic/Topic_08_Break_Continue.java
@@ -0,0 +1,10 @@
+package javaBasic;
+
+public class Topic_08_Break_Continue {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
