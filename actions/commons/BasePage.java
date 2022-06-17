37fd7f120db2f25e936813e4c6ef7fdf40bab2ea
Nguyen Thi Thanh <nguyen.thi.thanhb@b121451-lt.sun-asterisk.com>
2022-06-17 12:39:20 +0700

    Apply BasePage

--- actions/commons/BasePage.java
+++ actions/commons/BasePage.java
@@ -17,6 +17,10 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class BasePage {
 	
+	public static BasePage getBasePageObject() {
+		return new BasePage();
+	}
+	
 	public void openUrlPage(WebDriver driver, String urlPage) {
 		driver.get(urlPage);
 		
